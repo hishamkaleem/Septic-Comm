18d9cf09845f1571ddb8a09fd39d2b67ff9ebbf4
Hisham Kaleem <hishamkaleem27@gmail.com>
2025-05-21 18:54:07 -0400

    added AWS lambda function

---
+++ lambda.py
@@ -0,0 +1,26 @@
+#AWS lambda function to save to S3 bucket
+
+import json
+import boto3
+import uuid
+from datetime import datetime
+
+s3 = boto3.client('s3')
+bucket_name = ''  # Replace with own bucket
+
+def lambda_handler(event, context):
+    timestamp = datetime.utcnow().isoformat()
+    filename = f"Sensor_Data_{timestamp}_{uuid.uuid4().hex}.json"
+    
+    # Upload full Notehub event to S3
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=filename,
+        Body=json.dumps(event),
+        ContentType='application/json'
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': 'Data saved to Werl Bucket'
+    }
